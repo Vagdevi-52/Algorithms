aa32e2799ede53b96ec18a196b495557c6c818aa
vagdevi-52 <dhulipalavagdevi1952@email.com>
2021-07-07 22:22:15 -0700

    Added: Program For Finding The Permutation Of A String

---
+++ src/algorithms/Permutation.java
@@ -0,0 +1,38 @@
+package algorithms;
+
+public class Permutation 
+{
+	public static void main(String[] args)
+	{
+		String str = "mavaya"; 
+		int n = str.length(); //check the length of a string.
+		Permutation permutation = new Permutation();
+		permutation.permute(str, 0, n - 1);
+	}
+
+
+	private void permute(String str, int l, int r) //use  permute method  by checking the possible ways of a string. 
+	{
+		if (l == r)
+			System.out.println(str);
+		else
+		{
+			for (int i = l; i <= r; i++)
+			{
+				str = swap(str, l, i);
+				permute(str, l + 1, r);
+				str = swap(str, l, i);
+			}
+		}
+	}	
+	public String swap(String a, int i, int j) //use swap method  charcters at the  position
+	{
+		char temp;
+		char[] charArray = a.toCharArray();
+		temp = charArray[i];
+		charArray[i] = charArray[j];
+		charArray[j] = temp;
+		return String.valueOf(charArray);
+	}
+
+}
